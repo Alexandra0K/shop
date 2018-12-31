be614882c36683c006c5bc6da54912782af2e018
Alexandra0K <Alexandra_K@students.softuni.bg>
2018-12-31 18:47:35 +0200

    last

--- src/OnlineFashionBundle/Controller/AdminController.php
+++ src/OnlineFashionBundle/Controller/AdminController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace OnlineFashionBundle\Controller;
@@ -47,53 +46,3 @@ class AdminController extends Controller
             ['user'=>$user]);
     }
 }
-=======
-<?php
-
-namespace OnlineFashionBundle\Controller;
-
-use OnlineFashionBundle\Entity\User;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
-
-/**
- * @Route("admin")
- * Class AdminController
- * @package OnlineFashionBundle\Controller
- *
- */
-class AdminController extends Controller
-{
-    /**
-     * @Route("/", name="all_users")
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function indexAction()
-    {
-
-        $allUsers = $this
-            ->getDoctrine()
-            ->getRepository(User::class)
-            ->findAll();
-
-        return $this->render('admin/index.html.twig',
-            ['allUsers' => $allUsers]);
-    }
-
-    /**
-     * @Route("/user_profile/{id}", name="admin_user_profile")
-     * @param $id
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function userProfile($id)
-    {
-        $user = $this
-            ->getDoctrine()
-            ->getRepository(User::class)
-            ->find($id);
-
-        return $this->render('admin/user_profile.html.twig',
-            ['user'=>$user]);
-    }
-}
->>>>>>> fda21f45e5f6597f4f892dacfe0669d34d72f089
